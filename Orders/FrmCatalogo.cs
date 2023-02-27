95daa61833971c7936672f182c410858a1d0ca72
Uzann <Uzann@DESKTOP-EQ3MGO4>
2023-02-27 11:59:53 -0300

    Alterações em FrmCatalogo.cs

--- Orders/FrmCatalogo.cs
+++ Orders/FrmCatalogo.cs
@@ -34,52 +34,12 @@ namespace Orders
 
         private void CarregarCategorias()
         {
-
-            AcrescentarButtons(catDAO.ListarCat(string.Empty, false));
-
-            
-        }
-
-        private void DynamicButton_Click(object sender, EventArgs e)
-        {
-
-            if (Params.TryGetValue(sender, out Tuple<int, string> value))
-            {
-
-                //esse código é passível de melhorias
-
-                if (prodDAO.ListaProdCat(value.Item2).Rows.Count > 0)
-                {
-                    BtnVoltar.Visible = true;
-                    AcrescentarButtons(prodDAO.ListaProdCat(value.Item2));
-                }
-                else
-                {
-                    if (!listaitens.Contains(new Itenspedido(Convert.ToInt32(value.Item1), value.Item2)) && catDAO.ListarCat(value.Item2, true).Rows.Count == 0)
-                    {
-                        Itens item = new Itens();
-                        item.LblItem.Text = value.Item2.ToString();
-                        item.Lblid.Text = value.Item1.ToString();
-                        FlpItens.Controls.Add(item);
-                        BtnFinalizarpedido.Visible = true;
-
-                        listaitens.Add(new Itenspedido(Convert.ToInt32(value.Item1), value.Item2));
-                    }
-                    else
-                    {
-                        MessageBox.Show("Não existe nenhum produto cadastrado nessa categoria!");
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show(sender.ToString() + " not found.");
-            }
+            AcrescentarCategorias(catDAO.ListarCat(string.Empty, false));
         }
 
         public void AcrescentarItens(int id, string nome)
         {
-            if (!listaitens.Contains(new Itenspedido(Convert.ToInt32(id),nome)))
+            if (!listaitens.Contains(new Itenspedido(Convert.ToInt32(id), nome)))
             {
                 Itens item = new Itens();
                 item.LblItem.Text = nome.ToString();
@@ -87,10 +47,9 @@ namespace Orders
                 item.Tag = id;
                 FlpItens.Controls.Add(item);
                 BtnFinalizarpedido.Visible = true;
-                listaitens.Add(new Itenspedido(Convert.ToInt32(id),nome));
+                listaitens.Add(new Itenspedido(Convert.ToInt32(id), nome));
             }
         }
-
 
         public void Excluiritem(int id, string nome)
         {
@@ -120,76 +79,19 @@ namespace Orders
             i.ShowDialog();
         }
 
-        public void AcrescentarButtons(DataTable lista)
+        public void AcrescentarCategorias(DataTable lista)
         {
-
-            var orientation = SystemInformation.ScreenOrientation;
-            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
-            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
-
-         
+            // testando nova forma de aparecer as categorias
             FlpCategorias.Controls.Clear();
-            int i = 0;
+            int j = 0;
             while (FlpCategorias.Controls.Count < lista.Rows.Count)
             {
-                Button dynamicButton = new Button();
                 Catalogo catal = new Catalogo();
-                int x = screenWidth - (screenWidth * 30 / 100), y = screenHeight - (screenHeight * 30 / 100);
-
-                if (orientation.ToString() == "Angle0")
-                {
-                    dynamicButton.Width = (x / 5) - 5;
-                    dynamicButton.Height = (y / 3);
-                }
-                else if (orientation.ToString() == "Angle90")
-                {
-                    dynamicButton.Width = (x / 3) - 10;
-                    dynamicButton.Height = (y / 5);
-                }
-
-                dynamicButton.Text = lista.Rows[i]["nome"].ToString();
-
-              //  catal.Btncategoria.Text = lista.Rows[i]["nome"].ToString();
-                //try catch provisóirio
-                try
-                {
-                    string path = lista.Rows[i]["imagem"].ToString();
-
-                    if (File.Exists(path))
-                    {
-                        dynamicButton.BackgroundImage = Image.FromFile(path);
-                        dynamicButton.BackgroundImageLayout = ImageLayout.Stretch;
-                    }
-                }
-                catch
-                {
-
-                }
-              
-                dynamicButton.Font = new Font("Arial", 12);
-                dynamicButton.TextAlign = ContentAlignment.TopCenter;
-                Params.Add(dynamicButton, new Tuple<int, string>(Convert.ToInt32(lista.Rows[i]["ID"].ToString()), dynamicButton.Text));
-                dynamicButton.Click += DynamicButton_Click;
-                FlpCategorias.Controls.Add(dynamicButton);
-             //   FlpCategorias.Controls.Add(catal);
-
-                i++;
-            }
-
-
-           // testando nova forma de aparecer as categorias
-           FlpCategorias.Controls.Clear();
-           int j = 0;
-           while (FlpCategorias.Controls.Count < lista.Rows.Count)
-           {          
-               Catalogo catal = new Catalogo();            
                 catal.Btncategoria.Text = lista.Rows[j]["nome"].ToString();
-               catal.Tag = Convert.ToInt32(lista.Rows[j]["ID"].ToString());
-               FlpCategorias.Controls.Add(catal);
-               j++;
-           }
-
-
+                catal.Tag = Convert.ToInt32(lista.Rows[j]["ID"].ToString());
+                FlpCategorias.Controls.Add(catal);
+                j++;
+            }
         }
 
         public void AcrescentarProdutos(DataTable lista)
